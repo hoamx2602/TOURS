1fa0675bc72a26db25009dcc977a2e22a6733dfd
hoamx <hoamx.work@gmail.com>
2022-09-24 17:12:37 +0700

    fix checkout

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -15,8 +15,6 @@ exports.alerts = (req, res, next) => {
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
-  console.log('YYYYYYYYYYYYYYYYYYYYYYY');
-
   // 2) Build template
   // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
